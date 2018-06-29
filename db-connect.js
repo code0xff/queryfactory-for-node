15778207e0a141c16030811bc53afcc08a90a6be
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2018-06-29 09:10:23 +0900

    Update db-connect.js

--- db-connect.js
+++ db-connect.js
@@ -1,81 +1,68 @@
-const jsonHandler = require('./json-handler');
-const connection = require('./db-connect');
+const mysql = require('promise-mysql');
 
-let queryFactory = {};
+let pool;
 
-exports.createQueryFactory = (projectPath, configPath) => {
-    const config = jsonHandler.loadMapperConfig(projectPath, configPath);
-    connection.openConnection(config.connection);
-    queryFactory = jsonHandler.loadQueries(projectPath, config.mappers);
+exports.openConnection = (dbConfig) => {
+    pool = mysql.createPool(dbConfig);
 }
 
-exports.select = async (key, param) => {
-    const sql = getQuery('select', key, param);
-    console.log(sql);
-
-    const result = await connection.select(sql);
+exports.select = async (sql) => {
+    const conn = await pool.getConnection();
+    const result = await conn.query(sql).catch(error => {
+        conn.connection.release();
+        throw error;
+    });
+    conn.connection.release();
     return result;
-}   
-
-exports.insertSync = async (key, param) => {
-    const sql = getQuery('insert', key, param);
-    console.log(sql);
-
-    await connection.insertSync(sql);
 }
 
-exports.insert = (key, param) => {
-    const sql = getQuery('insert', key, param);
-    console.log(sql);
-
-    connection.insert(sql);
-}
- 
-exports.updateSync = async (key, param) => {
-    const sql = getQuery('update', key, param);
-    console.log(sql);
-
-    await connection.update(sql);
+exports.insertSync = async (sql) => {
+    await querySync(sql);
 }
 
-exports.update = (key, param) => {
-    const sql = getQuery('update', key, param);
-    console.log(sql);
-
-    connection.update(sql);
+exports.insert = (sql) => {
+    query(sql);
 }
 
-exports.deleteSync = async (key, param) => {
-    const sql = getQuery('delete', key, param);
-    console.log(sql);
-
-    await connection.delete(sql);
+exports.deleteSync = async (sql) => {
+    await querySync(sql);
 }
 
-exports.delete = (key, param) => {
-    const sql = getQuery('delete', key, param);
-    console.log(sql);
-
-    connection.delete(sql);
+exports.delete = (sql) => {
+    query(sql);
 }
 
-const insertParamIntoSQL = (sql, param) => {
-    if (param !== undefined) {
-        for (let key in param) {
-            sql = sql.replace(new RegExp('#{' + key + '}', 'gi'), "'" + param[key] + "'");
-        }
-    }
-    return sql;
+exports.updateSync = async (sql) => {
+    await querySync(sql);
 }
 
-const getQuery = (type, key, param) => {
-    if (queryFactory[type][key] === undefined) {
-        console.log(key + ' is not in ' + type + ' queries. please check mapper file.');
-        return;
-    }
-    return insertParamIntoSQL(queryFactory[type][key], param);
+exports.update = (sql) => {
+    query(sql);
 }
 
-exports.getQueryList = () => {
-    return queryFactory;
+const query = async (sql) => {
+    const conn = await pool.getConnection();
+    conn.query(sql)
+    .then((data) => {
+        // console.log(data);
+        conn.commit();
+        conn.connection.release();
+    })
+    .catch(error => {
+        conn.rollback();
+        conn.connection.release();
+        throw error;
+    });
+}
+
+const querySync = async (sql) => {
+    const conn = await pool.getConnection();
+    const result = await conn.query(sql).catch(error => {
+        conn.rollback();
+        conn.connection.release();
+        throw error;
+    });
+    // console.log(result);
+    conn.commit();
+    conn.connection.release();
 }
